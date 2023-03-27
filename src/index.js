64083016eeced04d1edb851f2345d103e61e3b2c
AmirHA <hasanabadi.am1380@gmail.com>
2023-03-27 07:40:39 -0700

    Session 8

--- src/index.js
+++ src/index.js
@@ -2,19 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const tick = ()=>{
-    const elem = (
-        <div>
+class Hello extends React.Component {
+    render() {
+        return (
             <h1>
                 This is a Clock
             </h1>
+        )
+    }
+}
+
+class Timer extends React.Component {
+    render() {
+        return (
             <h2>
                 Your local time is <span>{new Date().toLocaleTimeString()}</span>
             </h2>
-        </div>
-    )
-    ReactDOM.createRoot(document.getElementById('root')).render(elem);
+        )
+    }
 }
-setInterval(()=>{
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Hello/>
+                <Timer />
+            </div>
+        )
+    }
+}
+
+// ---------------------------------------
+// {let elem1 = new App()}
+// elem1.render() == <App/>
+// ---------------------------------------
+
+const tick = () => {
+    root.render(<App />)
+}
+
+setInterval(() => {
     tick()
 }, 1000)
+// ===================================================
