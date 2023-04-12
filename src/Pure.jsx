0c27b034f8ce87894feb1bca51f7d8584594c826
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-12 06:06:50 -0700

    session 25 - 2

--- src/Pure.jsx
+++ src/Pure.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { memo } from 'react';
 
-class Pure extends React.PureComponent{
-    render() {
-        console.log("pure");
-        return (
-             <h4 className='text-danger'>
-                {this.props.fName}
-             </h4>
-        );
-    }
+// class Pure extends React.PureComponent{
+//     render() {
+//         console.log("pure");
+//         return (
+//              <h4 className='text-danger'>
+//                 {this.props.fName}
+//              </h4>
+//         );
+//     }
+// }
+
+const Pure = (props) => {
+    console.log("pure");
+    return (
+        <h4 className='text-danger'>
+            {props.fName}
+        </h4>
+    );
 }
 
-export default Pure;
+export default memo(Pure);
