c2fc6c235a91ed0eb2ecc13d156df993a90035d3
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-12 02:39:02 -0700

    session 22

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -1,26 +1,25 @@
+import { useContext } from "react";
+import { TaskContext } from "./TaskContext";
+
 const TaskItems = () => {
+    let { taskItem, setTaskItem } = useContext(TaskContext);
     return (
         <ul className="list-group">
-            <li className="list-group-item w-100 d-flex justify-content-between p-3 bg-dark">
-                <span className='text-light'>
-                    First Job
-                </span>
-                <span className='icon-wrapper d-flex justify-content-between'>
-                    <i className='fa fa-check text-success'></i>
-                    <i className='fa fa-times text-warning'></i>
-                    <i className='fa fa-trash text-danger'></i>
-                </span>
-            </li>
-            <li className="list-group-item w-100 d-flex justify-content-between p-3 bg-secondary">
-                <span className='text-light'>
-                    Second Job
-                </span>
-                <span className='icon-wrapper d-flex justify-content-between'>
-                    <i className='fa fa-check text-success'></i>
-                    <i className='fa fa-times text-warning'></i>
-                    <i className='fa fa-trash text-danger'></i>
-                </span>
-            </li>
+            {
+                taskItem.map(e => (
+                    <li className={`list-group-item w-100 d-flex justify-content-between p-3 ${e.isDone ? "bg-success" : "bg-dark"}`}>
+                        <span className='text-light'>
+                            {e.title}
+                        </span>
+                        <span className='icon-wrapper d-flex justify-content-between'>
+                            {
+                                e.isDone ? <i className='fa fa-times text-warning'></i> : <i className='fa fa-check text-info'></i> 
+                            }
+                            <i className='fa fa-trash text-danger'></i>
+                        </span>
+                    </li>
+                ))
+            }
         </ul>
     )
 }
--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopForm from './TopForm';
 import TaskItems from './TaskItems';
+import { TaskContext } from './TaskContext';
 
 const App = () => {
+    const [taskItem, setTaskItem] = useState([
+        {
+            id: 1,
+            title: "First Job",
+            isDone: false
+        },
+        {
+            id: 2,
+            title: "Second Job",
+            isDone: true
+        },
+        {
+            id: 3,
+            title: "Third Job",
+            isDone: false
+        }
+    ])
     return (
         <div className="container">
             <div className='row d-flex justify-content-center'>
                 <div className='col-md-10 col-lg-8 col-xl-6'>
                     <div className='wrapper text-center p-3 my-4'>
-                        <TopForm/>
-                        <TaskItems/>
+                        <TaskContext.Provider value={{
+                            taskItem, 
+                            setTaskItem
+                        }}>
+                            <TopForm />
+                            <TaskItems />
+                        </TaskContext.Provider>
                     </div>
                 </div>
             </div>
