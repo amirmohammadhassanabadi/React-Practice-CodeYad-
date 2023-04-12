1e1d96815e993d6f2cd987680f9ad43e05f87e2b
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-12 03:45:14 -0700

    session 23

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -3,25 +3,35 @@ import { TaskContext } from "./TaskContext";
 
 const TaskItems = () => {
     let { taskItem, setTaskItem } = useContext(TaskContext);
-    return (
-        <ul className="list-group">
-            {
-                taskItem.map(e => (
-                    <li className={`list-group-item w-100 d-flex justify-content-between p-3 ${e.isDone ? "bg-success" : "bg-dark"}`}>
-                        <span className='text-light'>
-                            {e.title}
-                        </span>
-                        <span className='icon-wrapper d-flex justify-content-between'>
-                            {
-                                e.isDone ? <i className='fa fa-times text-warning'></i> : <i className='fa fa-check text-info'></i> 
-                            }
-                            <i className='fa fa-trash text-danger'></i>
-                        </span>
-                    </li>
-                ))
-            }
-        </ul>
-    )
+    if (taskItem.length > 0) {
+        return (
+            <ul className="list-group">
+                {
+                    taskItem.map(e => (
+                        <li className={`list-group-item w-100 d-flex justify-content-between p-3 ${e.isDone ? "bg-success" : "bg-dark"}`}>
+                            <span className='text-light'>
+                                {e.title}
+                            </span>
+                            <span className='icon-wrapper d-flex justify-content-between'>
+                                {
+                                    e.isDone ? <i className='fa fa-times text-warning shadow_warning'></i> : <i className='fa fa-check text-info shadow_info'></i>
+                                }
+                                <i className='fa fa-trash text-danger'></i>
+                            </span>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }else{
+        return(
+            <div className="w-100 py-2 bg-dark border border-danger rounded">
+                <h3 className="text-danger">
+                    0 Tasks to do
+                </h3>
+            </div>
+        )
+    }
 }
 
 export default TaskItems;
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,15 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useContext, useState } from "react";
+import { TaskContext } from "./TaskContext";
 
 const TopForm = () => {
+    const [task, setTask] = useState("")
+    const { taskItem, setTaskItem } = useContext(TaskContext)
+
+    const inputTaskHandler = (e) => {
+        setTask(e.target.value);
+        console.log(task);
+    }
+
+    const saveTask = (e) => {
+        e.preventDefault();
+        setTaskItem([...taskItem, {
+            id: taskItem.length + 1,
+            title: task,
+            isDone: false
+        }])
+    }
+
     return (
         <Fragment>
             <h1 className='text-info my-2 text_shadow'>
                 Wellcome to React
             </h1>
-            <form action='#' className='my-4'>
+            <form className='my-4'>
                 <div className='d-flex form-group'>
-                    <input type='text' className='form-control me-1'></input>
-                    <input type='submit' className='btn btn-info'></input>
+                    <input type='text' className='form-control me-1 input' onChange={inputTaskHandler} />
+                    <input type='submit' className='btn btn-info' onClick={saveTask} />
                 </div>
             </form>
         </Fragment>
