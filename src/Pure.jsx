a3b6470903017d8f6182a19959a834bf344d7940
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-13 09:12:56 -0700

    session 27 - 1

--- src/Pure.jsx
+++ src/Pure.jsx
@@ -1,41 +1,29 @@
-import React, { Fragment, createRef } from 'react';
+import React, { Fragment, createRef, useEffect, useRef, useState } from 'react';
 
-class Pure extends React.PureComponent {
-    constructor(){
-        super();
-        this.state = {
-            name: ""
-        }
-        this.counter = 0;
-        this.myInput = createRef()
-    }
+let counter = 0;
 
-    changeName = ()=>{
-        this.setState({
-            name: this.myInput.current.value
-        })
+const Pure = () => {
+    const [name, setName] = useState("");
+    const myInput = useRef();
+    const changeName = () => {
+        setName(myInput.current.value);
     }
-
-    componentDidMount(){
-        this.myInput.current.focus()
-    }
-
-    render() {
-        // console.log(this.myInput);
-        return (
-            <Fragment>
-                <h1 className='text-light my-2'>
-                    Guess the Word
-                </h1>
-                <input ref={this.myInput} type='text' id='input' className='form-control mt-4' />
-                <div className="btn-wrapper mt-4">
-                    <button className='btn  me-2' onClick={()=>{this.myInput.current.value = ""}}>Hide</button>
-                    <button className='btn' onClick={this.changeName}>Submit</button>
-                </div>
-                <h5 className='py-3'>{this.counter++}</h5>
-            </Fragment>
-        );
-    }
+    useEffect(() => {
+        myInput.current.focus();
+    }, [])
+    return (
+        <Fragment>
+            <h1 className='text-light my-2'>
+                Guess the Word
+            </h1>
+            <input ref={myInput} type='text' id='input' className='form-control mt-4' />
+            <div className="btn-wrapper mt-4">
+                <button className='btn  me-2' onClick={() => { myInput.current.value = "" }}>Hide</button>
+                <button className='btn' onClick={changeName}>Submit</button>
+            </div>
+            <h5 className='py-3'>{counter++}</h5>
+        </Fragment>
+    );
 }
 
 export default Pure;
